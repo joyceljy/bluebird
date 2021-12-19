7a516dca1eca3f673978928ff726eef512bd74d2
daumenas <daumius@gmail.com>
2021-12-19 23:52:03 +0100

    added txt of tweets commented on what hashtags used

--- examples/search.py
+++ examples/search.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 
 from bluebird import BlueBird
 
 query = {
     'fields': [
-        {'items': ['brunn3is']},
-    ]
-}
+        # {'items': ['NBA'], 'target': 'hashtag'}
+        # {'items': ['TourdeFrance'], 'target': 'hashtag'}
+        # {'items': ['UEFANationsLeague'], 'target': 'hashtag'}
+        # {'items': ['csgo'], 'target': 'hashtag'}
+        # {'items': ['leagueoflegends'], 'target': 'hashtag'}
+        # {'items': ['dota2'], 'target': 'hashtag'}
 
-for tweet in BlueBird().search(query, deep=True):
+    ],
+    'since': '2021-09-01',
+    'until': '2021-12-18'
+}
+f = open("../data/lolworld.txt", "a+")
+for tweet in BlueBird().search(query):
     print(tweet)
+    f.write(json.dumps(tweet))
